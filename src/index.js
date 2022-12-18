82b2ee9242edac90b90377e5cf0065656ac659a1
Daniel Moschnitzka <d.moschnitzka@gmail.com>
2022-12-18 15:04:58 +0100

    new branch with firebase

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import {initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAjRsdoue-naZ3PcaGiJpp1qF8HJA5OsCs",
+  authDomain: "newsletter-signup-700db.firebaseapp.com",
+  projectId: "newsletter-signup-700db",
+  storageBucket: "newsletter-signup-700db.appspot.com",
+  messagingSenderId: "563176827774",
+  appId: "1:563176827774:web:eed538ad6c5dd9cd51e12f",
+  measurementId: "G-TXCL2J99VZ"
+};
+
+const app = initializeApp(firebaseConfig);
